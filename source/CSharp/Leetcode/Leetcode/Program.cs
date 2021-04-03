1f4638f5152cbdc619262d79f47158e671ad12e5
Vladislav <50212442+vsnedro@users.noreply.github.com>
2021-04-03 23:52:35 +0300

    Added solution.

---
+++ source/CSharp/Leetcode/Leetcode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
